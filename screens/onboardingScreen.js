054f00935dedce1aec8f08b76736411b21eda78a
syedmohammadammar <syed.m.ammar@hotmail.com>
2020-12-25 03:43:23 +0500

    added react-native onboarding swiper lib and the basic code

--- screens/onboardingScreen.js
+++ screens/onboardingScreen.js
@@ -1,3 +1,18 @@
 import { forModalPresentationIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
 import React from 'react';
 import { view, Button, Text, StyleSheet} from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+
+<Onboarding
+  pages={[
+    {
+      backgroundColor: '#fff',
+      image: <Image source={require('./images/circle.png')} />,
+      title: 'Onboarding',
+      subtitle: 'Done with React Native Onboarding Swiper',
+    },
+    ...
+  ]}
+/>
