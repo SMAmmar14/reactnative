6186f11d0bf6c39e2966cd5697138b2e657dbcfa
syedmohammadammar <syed.m.ammar@hotmail.com>
2020-12-24 19:05:01 +0500

    changed app.js code and added onboarding screen in render return

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/stack';
+
+
 
 //import Screens
 import OnboardingScreen from './screens/onboardingScreen';
@@ -12,12 +16,7 @@ import {
   StatusBar
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const AppStack = createStackNavigator();
 
 export default class App extends Component {
 
@@ -26,43 +25,17 @@ export default class App extends Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="#4F6D7A"
-        />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+   <NavigationContainer>
+     <AppStack.Navigator
+     headermode="none">
+
+       <AppStack.Screen name="onboardingScreen" Component={OnboardingScreen}/>
+
+       
+       
+     </AppStack.Navigator>
+   </NavigationContainer>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4F6D7A',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#F5FCFF',
-     fontFamily: "Montserrat-Bold",
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#F5FCFF',
-    marginBottom: 5,
-      fontFamily: "Montserrat-Regular",
-  },
-});
