796634398faf87bd4369dcedd7614acba703dd61
syedmohammadammar <syed.m.ammar@hotmail.com>
2020-12-25 03:51:03 +0500

    changed the content of all sliders according to zeplin artboard

--- screens/onboardingScreen.js
+++ screens/onboardingScreen.js
@@ -1,6 +1,6 @@
 import { forModalPresentationIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/CardStyleInterpolators';
 import React from 'react';
-import { view, Button, Text, StyleSheet} from 'react-native';
+import { view, Button, Text, Image, StyleSheet} from 'react-native';
 
 import Onboarding from 'react-native-onboarding-swiper';
 
@@ -16,14 +16,14 @@ import Onboarding from 'react-native-onboarding-swiper';
       {
       backgroundColor: '#fff',
       image: <Image source={require('../assets/img/slider2.png')} />,
-      title: 'Enabling Collaboration',
-      subtitle: 'We connect local shoppers to online buyers',
+      title: 'Peer Up',
+      subtitle: 'Going home? Ready to help? Drop Orders Get Compensated',
     },
       {
       backgroundColor: '#fff',
       image: <Image source={require('../assets/img/slider3.png')} />,
       title: 'Enabling Collaboration',
-      subtitle: 'We connect local shoppers to online buyers',
+      subtitle: 'Busy at home? Need groceries quickly? Want low service fees 0% Mark-up as well? Just make a list',
     },
   ]}
 />
